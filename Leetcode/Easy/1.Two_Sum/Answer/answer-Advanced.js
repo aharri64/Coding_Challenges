6c7eaf4fb07175d17aa142417b082555e3173067
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-26 13:47:18 -0600

    various answers

---
+++ Leetcode/Easy/1.Two_Sum/Answer/answer-Advanced.js
@@ -0,0 +1,15 @@
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+var twoSum = (nums, target) => {
+    let storage = {};
+    for ( let [index, num] of nums.entries()) {
+        if (storage[num] !== undefined) 
+        return [storage[num], index];
+        storage[target-num] = index;
+    }
+};
---
+++ Leetcode/Easy/1.Two_Sum/Answer/myAnswer.js
@@ -0,0 +1,15 @@
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+let solution = [];
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        solution = target - nums[i];
+        if (solution === )
+    }
+};
+numbers = [2,7,11,15];
+
+twoSum(numbers, 9)
