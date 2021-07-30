68b42425e906e71928373329d0f95b804dc0f420
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-29 20:07:09 -0600

    new answet for 7. revered integer

---
+++ Leetcode/Easy/7.Reverse_Integer/Answer/terribleWhitenboardAnswer.js
@@ -0,0 +1,24 @@
+/*
+ * @param {number} x
+ * @return {number}
+*/
+var reverse = function (x) {
+    let negative = x < 0;
+    let reversed = 0;
+
+    if (negative) {
+        x *= -1 ;
+        // MAKE IT POSITIVE
+    }
+
+    while (x > 0) {
+        reversed = (reversed * 10) + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    if (reversed > (2 ** 31 - 1)) {
+        return 0;
+    }
+
+    return negative ? (reversed * -1) : reversed;
+};
