392af459997e90ff2b7f7d3a2faeaf6fa70e656d
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-11-02 22:51:33 -0600

    palindrome

---
+++ Leetcode/Easy/8.Palindrome_Number/Answer/santamariaCodeAnswer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+// METHOD 1
+//     let reversed = x.toString().split('').reverse().join();
+//     return (x.toString() === reversed)
+
+// METHOD 2
+    let reversedStr = '';
+    let xStr = x.toString()
+
+    for (let i of xStr) {
+        reversedStr = i + reversedStr
+    }
+    return(reversedStr === xStr)
+
+};
